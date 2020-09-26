6dd007b15107e8a17f141d8e2df58b45648cf80a
hexu <mailhexu@gmail.com>
2020-09-26 11:58:59 +0200

    add link to TB2J in readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 The TB2J interface to OpenMX.
 For the usage of TB2J, see:
 - https://readthedocs.org/projects/tb2j/
+The link to TB2J:
+- https://github.com/mailhexu/TB2J
--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ __version__ = "0.2.0"
 
 long_description = """Interface of TB2J to OpenMX. 
 TB2J is a Python package aimed to compute automatically the magnetic interactions (superexchange  and Dzyaloshinskii-Moriya) between atoms of magnetic crystals from DFT Hamiltonian based on Wannier functions or Linear combination of atomic orbitals. It uses the Green's function method and take the local rigid spin rotation as a perturbation. The package can take the output from Wannier90, which is interfaced with many density functional theory codes or from codes based on localised orbitals. A minimal user input is needed, which allows for an easily integration into a high-throughput workflows.
-TB2J is available at <https://github.com/mailhexu/TB2J>
-For more information, see the documentation on
- <https://tb2j.readthedocs.io/en/latest/>
-
  """
 
 setup(
